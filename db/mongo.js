fd4b06ae782d03c632fef6b2261ac29fda7f5e3c
Khenth <cj.xavier124@gmail.com>
2022-08-26 23:29:55 -0500

    bd2

--- db/mongo.js
+++ db/mongo.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 
-const uri = "mongodb://localhost/skycap";
+const uri = "mongodb://127.0.0.1:27017/skycap";
 
 mongoose.connect(uri, {
   useNewUrlParser: true,
